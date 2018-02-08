c8d1cd9ca2d01c37478665656623586507e67cc1
dubuqug <dubuqug@wwu.edu>
2018-02-08 13:26:08 -0800

    added raycast to check if player can jump

--- Assets/Scripts/Jumper.cs
+++ Assets/Scripts/Jumper.cs
@@ -7,6 +7,7 @@ public class Jumper : MonoBehaviour {
 	public float JumpV;
 	public GameObject player;
 	private Rigidbody rb;
+	private bool grounded;
 
 	// Update is called once per frame
 
@@ -15,7 +16,11 @@ public class Jumper : MonoBehaviour {
 	}
 
 	void Update () {
-		if (Input.GetButtonDown ("Jump") && rb.velocity.y == 0) {
+		Ray downRay = new Ray (player.transform.position, Vector3.down);
+		RaycastHit hit;
+		Physics.Raycast (downRay, out hit);
+		grounded = hit.distance == 0.5;
+		if (Input.GetButtonDown ("Jump") && grounded) {
 			rb.velocity = Vector3.up * JumpV;
 		}
 	}
